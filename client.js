8f5df8adb7df45fb64282273c853519b77c371c9
helena <helena@helenas-MacBook-Pro.local>
2024-02-14 14:12:56 -0800

    update

---
+++ client.js
@@ -0,0 +1,35 @@
+let string1 = document.querySelector("#first-string");
+let string2 = document.querySelector("#second-string");
+
+let stringOne = "Banana" 
+string1.textContent= stringOne;
+
+let stringTwo = "This is fruit"
+string2.textContent= stringTwo;
+
+let number1 = document.querySelector("#first-number");
+let number2 = document.querySelector("#second-number");
+
+let numberOne = "27"
+number1.textContent= numberOne;
+
+let numberTwo = "90"
+number2.textContent= numberTwo;
+
+let arry1 = document.querySelector("#first-array");
+let array2 = document.querySelector("#second-array");
+
+let arrayOne = "This is Apple" 
+arry1.textContent = arrayOne;
+
+let arrayTwo = "Hello,apple"
+array2.textContent = arrayTwo;
+
+let object1 = document.querySelector("#first-object");
+let object2 = document.querySelector("#second-object");
+
+let objectOne = "Google"
+object1.textContent = objectOne;
+
+let obejectTwo = "Yes, this is website";
+object2.textContent= obejectTwo;
